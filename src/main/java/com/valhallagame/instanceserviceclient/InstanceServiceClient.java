e60973e894760f245b8bf6ae00c502d92364605a
Mio Engel <mioengel@gmail.com>
2017-12-16 12:44:10 +0100

    added deactivate instance

--- src/main/java/com/valhallagame/instanceserviceclient/InstanceServiceClient.java
+++ src/main/java/com/valhallagame/instanceserviceclient/InstanceServiceClient.java
@@ -6,6 +6,7 @@ import com.valhallagame.common.DefaultServicePortMappings;
 import com.valhallagame.common.RestCaller;
 import com.valhallagame.common.RestResponse;
 import com.valhallagame.instanceserviceclient.message.ActivateInstanceParameter;
+import com.valhallagame.instanceserviceclient.message.DeactivateInstanceParameter;
 import com.valhallagame.instanceserviceclient.message.GetPlayerSessionAndConnectionParameter;
 import com.valhallagame.instanceserviceclient.message.SessionAndConnection;
 
@@ -44,5 +45,10 @@ public class InstanceServiceClient {
 	public RestResponse<String> activateInstance(String gameSessionId, String address, int port) throws IOException {
 		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/activate-instance",
 				new ActivateInstanceParameter(gameSessionId, address, port), String.class);
+	}
+
+	public RestResponse<String> deactivateInstance(String gameSessionId) throws IOException {
+		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/deactivate-instance",
+				new DeactivateInstanceParameter(gameSessionId), String.class);
 	}
 }
