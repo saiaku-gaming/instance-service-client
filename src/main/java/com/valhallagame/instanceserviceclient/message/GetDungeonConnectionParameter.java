7d62c0ad8e450897ccb54bf5a60b1889e8fb270d
Mikael Berglund <mikbe@kth.se>
2017-12-25 22:29:00 +0100

    Lots of validation

--- src/main/java/com/valhallagame/instanceserviceclient/message/GetDungeonConnectionParameter.java
+++ src/main/java/com/valhallagame/instanceserviceclient/message/GetDungeonConnectionParameter.java
@@ -1,5 +1,7 @@
 package com.valhallagame.instanceserviceclient.message;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetDungeonConnectionParameter {
+	@NotNull
 	private String username;
+	
+	@NotNull
 	private String gameSessionId;
+	
+	@NotNull
 	private String version;
 }
