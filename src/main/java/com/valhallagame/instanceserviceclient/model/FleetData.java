6807e75e4221c634782cf25cd894841036737f02
Mio Engel <mioengel@gmail.com>
2018-06-02 17:28:22 +0200

    Fixed missing field

--- src/main/java/com/valhallagame/instanceserviceclient/model/FleetData.java
+++ src/main/java/com/valhallagame/instanceserviceclient/model/FleetData.java
@@ -1,9 +1,14 @@
 package com.valhallagame.instanceserviceclient.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FleetData {
 	private String version;
 	private String status;
+	private int instanceCount;
 }
