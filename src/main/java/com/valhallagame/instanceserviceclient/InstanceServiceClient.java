6c967e853c98c7d9b37966e1940716518d7874cc
Mikael Berglund <mikbe@kth.se>
2018-10-02 23:45:18 +0200

    Small cleanup on clients

--- src/main/java/com/valhallagame/instanceserviceclient/InstanceServiceClient.java
+++ src/main/java/com/valhallagame/instanceserviceclient/InstanceServiceClient.java
@@ -1,113 +1,99 @@
 package com.valhallagame.instanceserviceclient;
 
-import java.io.IOException;
-import java.util.List;
-
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.valhallagame.common.AbstractServiceClient;
 import com.valhallagame.common.DefaultServicePortMappings;
-import com.valhallagame.common.RestCaller;
 import com.valhallagame.common.RestResponse;
-import com.valhallagame.instanceserviceclient.message.ActivateInstanceParameter;
-import com.valhallagame.instanceserviceclient.message.AddLocalInstanceParameter;
-import com.valhallagame.instanceserviceclient.message.GetAllPlayersInSameInstanceParameter;
-import com.valhallagame.instanceserviceclient.message.GetDungeonConnectionParameter;
-import com.valhallagame.instanceserviceclient.message.GetHubParameter;
-import com.valhallagame.instanceserviceclient.message.GetRelevantDungeonsParameter;
-import com.valhallagame.instanceserviceclient.message.InstancePlayerLoginParameter;
-import com.valhallagame.instanceserviceclient.message.InstancePlayerLogoutParameter;
-import com.valhallagame.instanceserviceclient.message.StartDungeonParameter;
-import com.valhallagame.instanceserviceclient.message.UpdateInstanceStateParameter;
+import com.valhallagame.instanceserviceclient.message.*;
 import com.valhallagame.instanceserviceclient.model.FleetData;
 import com.valhallagame.instanceserviceclient.model.InstanceData;
 import com.valhallagame.instanceserviceclient.model.RelevantDungeonData;
 import com.valhallagame.instanceserviceclient.model.SessionAndConnectionData;
 
-public class InstanceServiceClient {
+import java.io.IOException;
+import java.util.List;
 
-	private static RestCaller restCaller = new RestCaller();
+public class InstanceServiceClient extends AbstractServiceClient {
 
 	private static InstanceServiceClient instanceServiceClient;
 
-	private String instanceServiceServerUrl = "http://localhost:" + DefaultServicePortMappings.INSTANCE_SERVICE_PORT;
-
 	private InstanceServiceClient() {
+		serviceServerUrl = "http://localhost:" + DefaultServicePortMappings.INSTANCE_SERVICE_PORT;
 	}
 
-	public static void init(String instanceServiceServerUrl) {
+	public static void init(String serviceServerUrl) {
 		InstanceServiceClient client = get();
-
-		client.instanceServiceServerUrl = instanceServiceServerUrl;
+		client.serviceServerUrl = serviceServerUrl;
 	}
 
 	public static InstanceServiceClient get() {
 		if (instanceServiceClient == null) {
 			instanceServiceClient = new InstanceServiceClient();
 		}
-
 		return instanceServiceClient;
 	}
 
 	public RestResponse<SessionAndConnectionData> getHub(String username, String clientVersion) throws IOException {
 		GetHubParameter getHubParameter = new GetHubParameter(username, clientVersion);
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/get-hub", getHubParameter,
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/get-hub", getHubParameter,
 				SessionAndConnectionData.class);
 	}
 
 	public RestResponse<RelevantDungeonData> getRelevantDungeons(String username, String clientVersion)
 			throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/get-relevant-dungeons",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/get-relevant-dungeons",
 				new GetRelevantDungeonsParameter(username, clientVersion), RelevantDungeonData.class);
 	}
 
 	public RestResponse<String> activateInstance(String gameSessionId, String address, int port) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/activate-instance",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/activate-instance",
 				new ActivateInstanceParameter(gameSessionId, address, port), String.class);
 	}
 
 	public RestResponse<String> updateInstanceState(String gameSessionId, String state) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/update-instance-state",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/update-instance-state",
 				new UpdateInstanceStateParameter(gameSessionId, state), String.class);
 	}
 
 	public RestResponse<String> startDungeon(String username, String map, String version) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/start-dungeon",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/start-dungeon",
 				new StartDungeonParameter(username, map, version), String.class);
 	}
 
 	public RestResponse<String> instancePlayerLogin(String token, String gameSessionId) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/instance-player-login",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/instance-player-login",
 				new InstancePlayerLoginParameter(token, gameSessionId), String.class);
 	}
 
 	public RestResponse<String> instancePlayerLogout(String username, String gameSessionId) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/instance-player-logout",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/instance-player-logout",
 				new InstancePlayerLogoutParameter(username, gameSessionId), String.class);
 	}
 
 	public RestResponse<SessionAndConnectionData> getDungeonConnection(String username, String gameSessionId,
 			String version) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/get-dungeon-connection",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/get-dungeon-connection",
 				new GetDungeonConnectionParameter(username, gameSessionId, version), SessionAndConnectionData.class);
 	}
 
 	public RestResponse<List<InstanceData>> getAllInstances() throws IOException {
-		return restCaller.getCall(instanceServiceServerUrl + "/v1/instance/get-all-instances",
+		return restCaller.getCall(serviceServerUrl + "/v1/instance/get-all-instances",
 				new TypeReference<List<InstanceData>>() {
 				});
 	}
 
 	public RestResponse<String> addLocalInstance(AddLocalInstanceParameter input) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/add-local-instance", input, String.class);
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/add-local-instance", input, String.class);
 	}
 
 	public RestResponse<List<String>> getAllPlayersInSameInstance(String senderUsername) throws IOException {
-		return restCaller.postCall(instanceServiceServerUrl + "/v1/instance/get-all-players-in-same-instance",
+		return restCaller.postCall(serviceServerUrl + "/v1/instance/get-all-players-in-same-instance",
 				new GetAllPlayersInSameInstanceParameter(senderUsername), new TypeReference<List<String>>() {
 				});
 	}
 	
 	public RestResponse<List<FleetData>> getFleets() throws IOException {
-		return restCaller.getCall(instanceServiceServerUrl + "/v1/instance/get-fleets", new TypeReference<List<FleetData>>() {
+		return restCaller.getCall(serviceServerUrl + "/v1/instance/get-fleets", new TypeReference<List<FleetData>>() {
 				});
 	}
 }
